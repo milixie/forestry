3fb9e902c4361db8ec7b22e7909340b4c738251c
milixie <1435606568@qq.com>
2016-09-02 21:41:26 +0800

    npm and bower install and get files

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name forestryApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the forestryApp
+ */
+angular.module('forestryApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name forestryApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the forestryApp
+ */
+angular.module('forestryApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
